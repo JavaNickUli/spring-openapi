909da3c708713e10f091929ce2c77d8fb66becf3
JavaNickUli <java.nickuli@mail.ru>
2022-08-19 13:43:23 +0300

    app init

---
+++ src/main/java/uli/nick/springopenapi/SpringOpenApiApplication.java
@@ -0,0 +1,12 @@
+package uli.nick.springopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOpenApiApplication.class, args);
+    }
+}
